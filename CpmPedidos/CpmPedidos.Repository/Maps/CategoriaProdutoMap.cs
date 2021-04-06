1f1ad51a1daca6eb3e4781653774005358f5fb70
alexandreSafarPaim <alexandresafarpaim@gmail.com>
2021-04-06 15:49:55 -0300

    Repository and migrations created

---
+++ CpmPedidos/CpmPedidos.Repository/Maps/CategoriaProdutoMap.cs
@@ -0,0 +1,18 @@
+﻿using CpmPedidos.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CpmPedidos.Repository
+{
+    public class CategoriaProdutoMap : BaseDomainMap<CategoriaProduto>
+    {
+        public CategoriaProdutoMap() : base("tb_categoriaproduto") { }
+        public override void Configure(EntityTypeBuilder<CategoriaProduto> builder)
+        {
+            base.Configure(builder);
+
+            builder.Property(x => x.Nome).HasColumnName("nome").HasMaxLength(50).IsRequired();
+            builder.Property(x => x.Ativo).HasColumnName("ativo").IsRequired();
+        }
+    }
+}
