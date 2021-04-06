a8c5a4da1954561bf119e538a7e896073993c336
alexandreSafarPaim <alexandresafarpaim@gmail.com>
2021-04-06 15:52:42 -0300

    API and controller creation

---
+++ CpmPedidos/CpmPedidos.API/Controllers/AppBaseController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace CpmPedidos.API.Controllers
+{
+    public class AppBaseController : ControllerBase
+    {
+        protected readonly IServiceProvider ServiceProvider;
+        public AppBaseController(IServiceProvider serviceProvider)
+        {
+            ServiceProvider = serviceProvider;
+        }
+ 
+    }
+}
