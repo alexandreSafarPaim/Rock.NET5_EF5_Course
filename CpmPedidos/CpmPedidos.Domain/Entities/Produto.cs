6a5f9aac61fe75fab76dda9b39a8a467785d631d
alexandreSafarPaim <alexandresafarpaim@gmail.com>
2021-04-06 15:47:30 -0300

    Create Entities

---
+++ CpmPedidos/CpmPedidos.Domain/Entities/Produto.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace CpmPedidos.Domain
+{
+    public class Produto : BaseDomain , IExibivel
+    {
+        public string Nome { get; set; }
+        public string Codigo { get; set; }
+        public string Descricao { get; set; }
+        public decimal Preco { get; set; }
+
+        public int IdCategoria { get; set; }
+        public virtual CategoriaProduto CategoriaProduto { get; set; }
+
+        public virtual List<Imagem> Imagens { get; set; }
+
+        public virtual List<PromocaoProduto> PromocoesProdutos{ get; set; }
+
+        public virtual List<Combo> Combos { get; set; }
+
+        public bool Ativo { get; set; }
+
+    }
+}
